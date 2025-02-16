0d35a42ce36d54ae38d897efddf5da4d7a7e64d9
Jemerson Maia <110849015+jemersonln5@users.noreply.github.com>
2025-02-16 15:56:41 -0300

    503 Issue (#1)

--- spec/address_validate/api_spec.rb
+++ spec/address_validate/api_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe AddressValidate::API do
   let(:usps_response) { AddressValidate::Test::Fixtures.load_fixture('api_response_success.xml') }
-  let(:response) { double(body: usps_response) }
+  let(:response) { double(code: '200', body: usps_response) }
   before { allow(Net::HTTP).to receive(:post_form).and_return(response) }
 
   it 'returns a response object' do
